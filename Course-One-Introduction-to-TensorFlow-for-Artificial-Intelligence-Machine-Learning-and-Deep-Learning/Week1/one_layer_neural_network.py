8f3316e12b40687e72f97ebb72fdd01551a998ca
Rahul Singh <rsembedded@gmail.com>
2020-11-08 20:36:13 +0530

    initial

---
+++ Course-One-Introduction-to-TensorFlow-for-Artificial-Intelligence-Machine-Learning-and-Deep-Learning/Week1/one_layer_neural_network.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+
+
+# define the neural network architecture
+
+model = tf.keras.Sequential(keras.layers.Dense(units=1 , input_shape=[1]))
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+# feed some data
+
+xs = np.array([-1.0,  0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
+
+# train model
+model.fit(xs,ys,epochs= 500)
+print(model.predict([10.0]))
