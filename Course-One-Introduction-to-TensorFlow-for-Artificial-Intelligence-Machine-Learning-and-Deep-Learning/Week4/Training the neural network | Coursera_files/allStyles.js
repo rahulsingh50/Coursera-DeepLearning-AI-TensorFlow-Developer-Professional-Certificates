8d50b2f97ded3f6421ea975227f55cadadc68b52
Rahul Singh <rsembedded@gmail.com>
2020-11-19 16:30:06 +0530

    4 weelk data

---
+++ Course-One-Introduction-to-TensorFlow-for-Artificial-Intelligence-Machine-Learning-and-Deep-Learning/Week4/Training the neural network | Coursera_files/allStyles.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["allStyles"],[]]);
+//# sourceMappingURL=allStyles.b048a24c79548de70838.js.map
---
+++ Course-One-Introduction-to-TensorFlow-for-Artificial-Intelligence-Machine-Learning-and-Deep-Learning/Week4/Week 4 Quiz | Coursera_files/allStyles.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["allStyles"],[]]);
+//# sourceMappingURL=allStyles.85d0223e8389bee08601.js.map
